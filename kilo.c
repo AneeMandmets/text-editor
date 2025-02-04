9410415d2d92703168d8d6c41ae48801ec586c31
AneeMandmets <anee.mandmets@gmail.com>
2025-02-04 15:07:23 +0200

    Fixes

--- kilo.c
+++ kilo.c
@@ -33,35 +33,41 @@ void enableRawMode() {
   if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) unalive("tcgetattr");     // Read current terminal attributes into a struct
   atexit(disableRawMode);                                                     // Calls disableModeRaw on program exit
   struct termios raw = orig_termios;                  
-  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);                   // Fixes ctrl-m, Disables ctrl-s and ctrl-q
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP);                   // Fixes ctrl-m, Disables ctrl-s and ctrl-q
+  raw.c_iflag &= ~(IXON);
   raw.c_oflag &= ~(OPOST);                                                    // Turns off output processing
   raw.c_cflag |= (CS8);                                                       // Sets character size to 8 bits per byte
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);                            // Modify the struct, Turn off canonical mode, disables ctrl-v, disables ctrl-c and ctrl-z
   raw.c_cc[VMIN] = 0;                                                         // Sets minimum number of bytes needed before read() can return, 0 means it returns as soon as there is any input to be read
   raw.c_cc[VTIME] = 1;                                                        // Sets maximum amount of time to wait before read() returns, 1/10 of a second
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) unalive("tcsetattr");   // Pass the modified struct to write the new terminal attributes back out
+  printf("Raw mode enabled\n"); // Debugging output
 }
 
 // Wait for one keypress and return it
 char editorReadKey() {
   int nread;
   char c;
-  while ((nread == read(STDIN_FILENO, &c, 1)) != 1) {
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
     if (nread == -1 && errno != EAGAIN) unalive("read");
   }
+  printf("Read key: %d\n", c); // Debugging output
   return c;
 }
 
 /* INPUT */
 
 // Waits for a keypress and processes it
-char editorProcessKeypress() {
+void editorProcessKeypress() {
   char c = editorReadKey();
-  switch (c)
-  {
-  case CTRL_KEY('Q'):
-    exit(0);
-    break;
+  switch (c) {
+    case CTRL_KEY('q'):
+    printf("Exiting program\n");
+      exit(0);
+      break;
+    default:
+      printf("Key pressed: %d\n", c);
+      break;
   }
 }
 
@@ -69,6 +75,7 @@ char editorProcessKeypress() {
 
 int main() {
   enableRawMode();
+  printf("Entering input loop\n"); // Debugging output
 
   while (1) {
     editorProcessKeypress();
