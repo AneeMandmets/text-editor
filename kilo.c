798dfe3a3e6dea62457730d7d256d6174ebed7fd
AneeMandmets <anee.mandmets@gmail.com>
2025-02-04 14:12:00 +0200

    Error handling

--- kilo.c
+++ kilo.c
@@ -1,6 +1,7 @@
 #include <ctype.h>    // iscntrl()
-#include <stdio.h>    // printf()
-#include <stdlib.h>   // atexit()
+#include <errno.h>    // EAGAIN, errno
+#include <stdio.h>    // printf(), perror()
+#include <stdlib.h>   // atexit(), exit()
 #include <termios.h>  /* 
                         struct termios, tcgetattr(), tcsetattr(), ECHO, TCSAFLUSH, ICANON, ISIG, IXON, IEXTEN, 
                         ICRNL, OPOST, RKINT, INPCK, ISTRIP, CS8, VMIN, VTIME
@@ -9,12 +10,17 @@
 
 struct termios orig_termios;
 
+void unalive(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) unalive("tcsetattr");
 }
  
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);                   // Read current terminal attributes into a struct
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) unalive("tcgetattr");                   // Read current terminal attributes into a struct
   atexit(disableRawMode);                                   // Calls disableModeRaw on program exit
   struct termios raw = orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // Fixes ctrl-m, Disables ctrl-s and ctrl-q
@@ -23,7 +29,7 @@ void enableRawMode() {
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);          // Modify the struct, Turn off canonical mode, disables ctrl-v, disables ctrl-c and ctrl-z
   raw.c_cc[VMIN] = 0;                                       // Sets minimum number of bytes needed before read() can return, 0 means it returns as soon as there is any input to be read
   raw.c_cc[VTIME] = 1;                                      // Sets maximum amount of time to wait before read() returns, 1/10 of a second
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);                 // Pass the modified struct to write the new terminal attributes back out
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) unalive("tcsetattr");                 // Pass the modified struct to write the new terminal attributes back out
 }
 
 int main() {
@@ -31,7 +37,7 @@ int main() {
 
   while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) unalive("read"); // Reads a single byte of input into the variable `c
     if (iscntrl(c)) {     // Checks if the character is a control character, nonprintable characters
       printf("%d\r\n", c);  // Prints the ASCII value of the character
     } else {
