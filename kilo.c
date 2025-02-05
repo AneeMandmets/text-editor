d43561eb0a3b713e911679e18cba39826d25ae24
AneeMandmets <anee.mandmets@gmail.com>
2025-02-05 14:31:55 +0200

    Added ability to use files

--- kilo.c
+++ kilo.c
@@ -1,8 +1,14 @@
 /* INCLUDES */
 
+// Define feature test macros to enable certain features in the standard library
+// These have to be before includes because they are used to enable certain features in the standard library
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>      // iscntrl()
 #include <errno.h>      // EAGAIN, errno
-#include <stdio.h>      // printf(), perror(), sscanf(), snprintf()
+#include <stdio.h>      // printf(), perror(), sscanf(), snprintf(), FILE, fopen(), getline(), fclose()
 #include <stdlib.h>     // atexit(), exit(), realloc(), free(), malloc(),
 #include <string.h>     // memcpy(), strlen()
 #include <sys/ioctl.h>  // struct winsize, ioctl(), TIOCGWINSZ
@@ -163,15 +169,26 @@ int getWindowSize(int *rows, int *cols) {
 
 /* FILE I/O */
 
-void editorOpen() {
-  char *line = "Hello, world!";
-  ssize_t linelen = 13;
-
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
-  E.row.chars[linelen] = '\0';
-  E.numrows = 1;  // The erow now contains a line of text
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");    // Open the file in read mode
+  if (!fp) unalive("fopen");          // If the file pointer is NULL, print an error message and exit  
+  
+  char *line = NULL;
+  size_t linecap = 0;                 // line capacity
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {                // if we are not at the end of the file
+    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen -1] == '\r'))
+      linelen--;
+    
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;                    // The erow now contains a line of text
+  }
+  free(line);
+  fclose(fp);
 }
 
 /* APPEND BUFFER */
@@ -203,7 +220,7 @@ void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     if (y >= E.numrows) { // Is the row we are drawing part of the text buffer or does it come after it?
-      if (y == E.screenrows / 3) {
+      if (E.numrows== 0 && y == E.screenrows / 3) {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
         if (welcomelen > E.screencols) welcomelen = E.screencols;
@@ -320,10 +337,12 @@ void initEditor() {
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) unalive("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
-  editorOpen();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
 
   while (1) {
     editorRefreshScreen();
